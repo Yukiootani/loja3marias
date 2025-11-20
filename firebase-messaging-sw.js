3d7ace39f596b57738a38723c18eb9e47fff93d4
Yukiootani <yukiocreditcard@hotmail.com>
2025-11-20 07:30:21 +0000

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,31 @@
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+
+// Suas chaves (As mesmas do index)
+const firebaseConfig = {
+    apiKey: "AIzaSyDtQK3dZKTqoTkXkwTUM2vkviVD41UoHpI",
+    authDomain: "loja3marias-50204.firebaseapp.com",
+    projectId: "loja3marias-50204",
+    storageBucket: "loja3marias-50204.firebasestorage.app",
+    messagingSenderId: "405330133487",
+    appId: "1:405330133487:web:606de41211df52245f761a",
+    measurementId: "G-TK877V6P0S"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+// Isso faz a notificação aparecer quando o site está fechado (Background)
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Mensagem recebida: ', payload);
+  
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'https://i.imgur.com/BIXdM6M.png', // Seu logo
+    click_action: payload.notification.click_action || 'https://loja3marias.netlify.app'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
